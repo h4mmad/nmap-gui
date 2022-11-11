05194b5714f714421ca233bbf854df515ffb6e49
h4mmad <mohammedhammad07@gmail.com>
2022-11-11 03:03:21 +0300

    spinner logic

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import './App.css';
-import Form from './components/Form';
-import Card from './components/Card';
-import Console from './components/Console';
-import Spinner from './components/Spinner';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import Form from "./components/Form";
+import Card from "./components/Card";
+import Console from "./components/Console";
+import Spinner from "./components/Spinner";
 
 function App() {
-
-  const [host, setHost] = useState('');
+  const [host, setHost] = useState("");
   const [spinner, setSpinner] = useState(false);
   const [apiResult, setApiResult] = useState([]);
-  const [consoleText, setConsoleText] = useState('');
+  const [consoleText, setConsoleText] = useState("");
+  const [error, setError] = useState(false);
 
   async function getAPIData(host) {
-    setSpinner(true);
-    const response = await fetch(`http://localhost:6969/${host}`);
-    const data = await response.text();
-
-    const arr = data.match(/[0-9]?[0-9]?[0-9]?[0-9]?\/(tcp|udp).*/mg);
-    const objArr = arr.map(e => {
-      const splitArr = e.split(/[ ]{1,}/);
-      const myObj = {
-        port: splitArr[0],
-        status: splitArr[1],
-        service: splitArr[2]
-      }
-      return myObj;
-    })
-    setApiResult(objArr);
-    setConsoleText(data);
-    setSpinner(false);
+    try {
+      setSpinner(true);
+      setError(false);
+      const response = await fetch(`http://localhost:6969/${host}`);
+      const data = await response.text();
+      const arr = data.match(/[0-9]?[0-9]?[0-9]?[0-9]?\/(tcp|udp).*/gm);
+      const objArr = arr.map((e) => {
+        const splitArr = e.split(/[ ]{1,}/);
+        const myObj = {
+          port: splitArr[0],
+          status: splitArr[1],
+          service: splitArr[2],
+        };
+        return myObj;
+      });
+      setApiResult(objArr);
+      setConsoleText(data);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+      setSpinner(false);
+    } finally {
+      setSpinner(false);
+    }
   }
 
-
-
   return (
-    <div className='container-sm'>
-
+    <div className="container-sm">
+      {error ? (
+        <div class="alert alert-warning" role="alert">
+          An error occured
+        </div>
+      ) : (
+        ""
+      )}
 
       <Form
         host={host}
@@ -47,21 +58,21 @@ function App() {
         getAPIData={getAPIData}
       />
 
+      {spinner ? <Spinner /> : ""}
 
-      {spinner ? <Spinner/> : ''}
+      {error | spinner ? (
+        ""
+      ) : (
+        <div>
+          <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
+            {apiResult.map((e) => (
+              <Card key={uuidv4()} individualCard={e} />
+            ))}
+          </div>
 
-      {spinner ? '' :
-      <div>
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
-          {apiResult.map(e => (<Card key={uuidv4()} individualCard={e}/>))}
+          {consoleText === "" ? "" : <Console consoleText={consoleText} />}
         </div>
-
-        {consoleText === '' ? '' : <Console consoleText={consoleText}/>}
-      </div>
-      }
-
-      
-
+      )}
     </div>
   );
 }
